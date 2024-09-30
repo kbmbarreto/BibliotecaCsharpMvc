0a1391dd6ba7c1a46051ab7695f7e46ab88d636f
cskmacedo <kleber.barreto@msn.com>
2024-09-30 10:03:49 -0300

    Adicionada documentação

---
+++ Migrations/20240920045421_CreatingDatabase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibliotecaCsharpMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatingDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Author = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    PublishingCompany = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    IsReaded = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    IsDigital = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Cover = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # BibliotecaCsharpMvc
-Sistema para gerenciamento do estoque de livros de uma biblioteca, em .NET MVC
+
+<p>Sistema para gerenciamento do estoque de livros de uma biblioteca, em .NET MVC.</p>
+
+## Ferramentas utilizadas
+
+- .NET 8
+- Bootstrap 5
+- Microsoft Entity Framework Core
+- Pomelo Entity Framework MySQL
+
+## Preparação do ambiente
+Para rodar o projeto, utlize a IDE que você mais se identifique **(no meu caso, utilizo o Visual Studio for Mac)**, em seguida, altere o arquivo **appsettings.json** para que o projeto se adeque ao seu servidor de **banco de dados**:
+
+**Exemplo do arquivo appsettings.json**:
+
+````java
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft.AspNetCore": "Warning"
+    }
+  },
+  "ConnectionStrings": {
+    "Connection": "Server=127.0.0.1;Port=3306;Database=books;User=root;Password=Dev2020;"
+  },
+  "AllowedHosts": "*"
+}
+````
+
+Em seguida, basta criar um banco de dados no MySql com o comando `Create schema {nomeDoBancoDeDados}`
+
+## Execução de testes unitários
+
+Em desenvolvimento.
+
+## Executando o projeto
+
+Para executar em um banco de dados recém criado, certifique-se de apagar (se houver) o conteúdo do diretório `Migrations` e siga com estes comandos:
+
+No Windows:
+
+`add-migration CreatingDatabase`
+`update-database`
+
+No Mac:
+
+`dotnet tool install --global dotnet-ef`
+`dotnet ef --version`
+`dotnet ef migrations add CreatingDatabase`
+`dotnet ef database update`
+
+## Links úteis.
+
+- [Excelente tutorial para criação de um sistema com ASP NET MVC](https://www.youtube.com/watch?v=w75Ok7ZhVaU&list=PL8l5r7-11AADIYThFwVOoM3L_m0Dfuiga)
